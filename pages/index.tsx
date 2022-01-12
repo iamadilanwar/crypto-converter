82ce537007a69fcf18d95bb6e2b53960b0bda5eb
iamadilanwar <iamadilanwar@gmail.com>
2022-01-12 18:41:20 +0530

    update1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from "next";
 import ConverterLayout from "../container/ConverterLayout";
 
 const Home: NextPage = () => {
-  return <ConverterLayout />;
+  return (
+  <ConverterLayout />);
 };
 
 export default Home;
