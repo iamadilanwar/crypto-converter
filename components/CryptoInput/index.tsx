9b2abaf3aa7d28c11925e34bd33360eb8c674d30
iamadilanwar <iamadilanwar@gmail.com>
2022-01-12 20:04:15 +0530

    number validation

--- components/CryptoInput/index.tsx
+++ components/CryptoInput/index.tsx
@@ -1,17 +1,28 @@
 import styles from "./styles.module.css";
 interface PropsType {
-    amount: number
-    onAmountChange: Function
-    currency: string
+  amount: number;
+  onAmountChange: Function;
+  currency: string;
 }
 
-const CryptoInput: React.FC<PropsType> = ({ amount, onAmountChange,currency}) => {
-
+const CryptoInput: React.FC<PropsType> = ({
+  amount,
+  onAmountChange,
+  currency,
+}) => {
   return (
     <div className={styles.group}>
-      <input type="number" value={amount} onChange={ev => onAmountChange(ev.target.value)} />
+      <input
+        value={amount}
+        onChange={(e) => {
+          const re = /^[0-9\b]+$/;
+          if (e.target.value === '' || re.test(e.target.value)) {
+            onAmountChange(e.target.value);
+          }
+       }}
+      />
       <p>{currency}</p>
     </div>
   );
-}
+};
 export default CryptoInput;
